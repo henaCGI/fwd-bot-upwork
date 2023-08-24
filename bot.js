a0dfaaac1530680accb408a4d30137c7066e78ed
Henok Ayenew <henacodes@gmail.com>
2023-08-25 02:15:19 +0400

    pff

--- bot.js
+++ bot.js
@@ -8,30 +8,26 @@ bot.command("start", (ctx) => {
   ctx.reply("Welcome");
 });
 bot.on("forward_date", (ctx) => {
-  if (ctx.chat.id !== parseInt(ADMIN_ID)) {
-    ctx.reply("Sorry, You are not authorized to do this ");
+  const { forward_from_chat, forward_from_message_id } = ctx.message;
+  if (forward_from_chat.id !== parseInt(CHANNEL_ID)) {
+    ctx.reply("Sir, you are forwarding this from a wrong channel");
   } else {
-    const { forward_from_chat, forward_from_message_id } = ctx.message;
-    if (forward_from_chat.id !== parseInt(CHANNEL_ID)) {
-      ctx.reply("Sir, you are forwarding this from a wrong channel");
-    } else {
-      groups.map(async (groupId) => {
-        console.log(forward_from_chat);
-        console.log(forward_from_message_id);
-        await ctx.telegram.forwardMessage(
-          groupId,
-          forward_from_chat.id,
-          forward_from_message_id
-        );
-      });
-    }
+    groups.map(async (groupId) => {
+      console.log(forward_from_chat);
+      console.log(forward_from_message_id);
+      await ctx.telegram.forwardMessage(
+        groupId,
+        forward_from_chat.id,
+        forward_from_message_id
+      );
+    });
   }
 });
 bot.launch({
-  webhook: {
+  /*   webhook: {
     domain: "https://forwarder-bot-demo.onrender.com",
     port: process.env.PORT || 5000,
-  },
+  }, */
 });
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
